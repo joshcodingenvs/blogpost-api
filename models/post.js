b2bdd400b1dd4bb6744f83dc8c9a888f12af9524
joshcodingenv <josh2023decoding@gmail.com>
2023-05-28 21:35:48 +0300

    Post model definition

---
+++ models/post.js
@@ -0,0 +1,54 @@
+// import required modules/packages
+const mongoose = require("mongoose");
+
+// post schema instance
+const postSchema = new mongoose.Schema({
+    author: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "user"
+    },
+    body: {
+        type: String,
+        required: true
+    },
+    img: {
+        type: Buffer,
+        contentType: String
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    views: {
+        type: Number,
+        default: 0
+    },
+    comments: [{
+        author: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "user"
+        },
+        body: {
+            type: String,
+            required: true
+        },
+        img: {
+            type: Buffer,
+            contentType: String
+        },
+        date: {
+            type: Date,
+            default: Date.now
+        },
+        views: {
+            type: Number,
+            default: 0
+        }
+    }]
+});
+
+// post schema model instance
+const Post = mongoose.model("post", postSchema);
+
+// export model instance
+module.exports = Post;
