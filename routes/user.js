bcfa1e621baa45c9e34c83c9b058dba64347d6f6
joshcodingenv <josh2023decoding@gmail.com>
2023-05-28 21:16:31 +0300

    Route handler testing

---
+++ routes/user.js
@@ -0,0 +1,13 @@
+// import required modules/packages
+const express = require("express");
+
+// router instance
+const router = express.Router();
+
+// app endpoints
+router.get("/users/test", (req, res)=>{
+    res.status(200).send("Test route handler working");
+});
+
+// export router instance
+module.exports = router;
